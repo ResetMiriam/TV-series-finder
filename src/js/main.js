03342e24e8d12452179128509edc1dbd64608858
MiriamVF <miriam.verduf@gmail.com>
2021-09-09 17:37:33 +0200

    initial fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const listSeries = document.querySelector(".searchResult");
+let series = [];
+
+fetch("https://api.tvmaze.com/shows")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    series = [data.palettes];
+  });
