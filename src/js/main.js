ebdc78c828554d1bd0d8d335967e833c1079b5c7
MiriamVF <miriam.verduf@gmail.com>
2021-09-11 12:06:38 +0200

    add favorites functions

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,17 @@
 
 //----------DECLARACIONES-------------//
 
-// input
+// input filter
 const searchText = document.querySelector(".js_search-input");
 // button
 const searchButton = document.querySelector(".js-search-button");
-// ul
+// ul -- donde se pintan series - selecciono donde se pintan
 const listSeries = document.querySelector(".js-searchResult");
 // form
 const requestPanel = document.querySelector(".js-form");
+
+// ul fav -- donde se pintan los favoritos
+const listFav = document.querySelector(".js-favListCompleted");
 
 // para utilizar
 let series = [];
@@ -17,11 +20,54 @@ let favorites = [];
 
 //----------FUNCIONES-------------//
 
-// busqueda con la info del input
+// OK busqueda con la info del input
 function handleSearch() {
   apiRequest(searchText.value);
 }
-// petición api
+
+//  OK comprobar si la clicada está en favoritos
+function addFavorites(ev) {
+  //sacar ID
+  const serieSelected = parseInt(ev.currentTarget.id);
+  // OK comparar arrayFavorites con ID
+  const serieClicked = series.find((idSerie) => {
+    return idSerie.id === serieSelected;
+  });
+  console.log(serieSelected);
+  const favAlready = favorites.findIndex((idFavorite) => {
+    return idFavorite.id === serieSelected;
+  });
+
+  if (favAlready === -1) {
+    favorites.push(serieClicked);
+  } else {
+    favorites.splice(favAlready, 1);
+  }
+  paintSeries();
+  paintFavorites();
+}
+
+function deleteFav(ev) {
+  const favSelected = parseInt(ev.currentTarget.id);
+  const favClicked = favorites.findIndex((idFav) => idFav.id === favSelected);
+  favorites.splice(favClicked, 1);
+  paintSeries();
+  paintFavorites();
+}
+
+// OK escuchar click en cada serie
+function listenClickSeries() {
+  const seriesCards = document.querySelectorAll(".js-searchResult_elem");
+  for (const serieCard of seriesCards)
+    serieCard.addEventListener("click", addFavorites);
+}
+// OK escuchar click en cada favorito
+function listenClickedFavorites() {
+  const favCards = document.querySelectorAll(".js-deleteCross");
+  for (const favCard of favCards) favCard.addEventListener("click", deleteFav);
+}
+
+// OK petición api
 function apiRequest(userSearch) {
   fetch("//api.tvmaze.com/search/shows?q=" + userSearch)
     .then((response) => response.json())
@@ -35,12 +81,39 @@ function apiRequest(userSearch) {
     });
   //
 }
-// plasmar series
+
+// OK
+function isFavorite(idSerie) {
+  //compruebo si la paleta que recibo por parámetro está en los favoritos
+  const favoriteFound = favorites.find((idFavorite) => {
+    // la dificultad de esta función interna del find es saber que tengo que comparar
+    // yo consolearía console.log(fav, palette) para ver los datos que debo comparar
+    return idFavorite.id === idSerie.id;
+  });
+  //find devuelve undefined si no lo encuentra, es decir sino esta en el array de favoritos
+  //retorno si está o no está en favoritos
+  if (favoriteFound === undefined) {
+    //retorno false cuando NO está favoritos
+    return false;
+  } else {
+    //retorno true cuando SI está favoritos
+    return true;
+  }
+}
+
+// OK plasmar series
 function paintSeries() {
   listSeries.innerHTML = "";
   let html = "";
+  let favClass = "";
   for (const serie of series) {
-    html = `<li id=${serie.id} class="searchResult_elem js-searchResult_elem">`;
+    const isFav = isFavorite(serie);
+    if (isFav) {
+      favClass = "serie--favorite";
+    } else {
+      favClass = "";
+    }
+    html = `<li id=${serie.id} class= "searchResult_elem js-searchResult_elem ${favClass}">`;
     if (null === serie.image) {
       html += `<img class="searchResult_elem-img" 
         src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" 
@@ -53,16 +126,30 @@ function paintSeries() {
     html += `<h3>${serie.name}</h3>`;
     html += `</li>`;
     listSeries.innerHTML += html;
+    listenClickSeries();
   }
 }
-// escuchar click en cada serie
-const seriesCards = document.querySelectorAll(".js-searchResult_elem");
-for (const serieCard of seriesCards) {
-  serieCard.addEventListener("click", showFavorites);
-}
 
-function showFavorites(ev) {
-  const favShows = ev.currentTarget;
+// OK plasmar favoritos
+function paintFavorites() {
+  let htmlFav = "";
+  for (const serie of favorites) {
+    htmlFav = `<li id=${serie.id} class="fav_section-list js-favSectionList">`;
+    htmlFav += `<button id ="${serie.id}" class="js-deleteCross faicon ">x</button>`;
+    if (null === serie.image) {
+      htmlFav += `<img class="fav_elem-img"
+          src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" 
+          title=${serie.name} alt=${serie.name}/>`;
+    } else {
+      htmlFav += `<img class="fav_elem-img"
+          src=${serie.image.medium}
+          title=${serie.name} alt=${serie.name}/>`;
+    }
+    htmlFav += `<h4 class="fav_elem-serieTitle"> ${serie.name}</h4>`;
+    htmlFav += `</li>`;
+    listFav.innerHTML += htmlFav;
+    listenClickedFavorites();
+  }
 }
 
 // preventDefault
@@ -76,3 +163,26 @@ function handleForm(ev) {
 searchButton.addEventListener("click", handleSearch);
 // cuando se envía el form -- preventDefault
 requestPanel.addEventListener("submit", handleForm);
+
+/* - [x] Diagrama de flujo (esquema)
+- [ ] Hacer la estructura del HTML, css minimo para trabajar
+- [] Pintar una serie
+  -  [ ] Seleccionar los elementos del HTML donde voy a pintar la lista de series
+  - [ ] Hacer peticion al servidor FETCH
+  - [ ] Parsear los datos del servidor .json()
+  - [ ] guardar en una variable global y
+  - [ ] pintar en el HTML los datos
+  - [ ] ponerlo bonito
+- [ ] Pintar muchas series
+- [ ] Seleccionar paletas favorita
+  - [ ] Escuchar evento sobre CLICK sobre las paletas
+  - [ ] Crear una funcion manejadora del evento
+  - [ ] Identificar la paleta clicada
+  - [ ] Añadir a un array de favorito la paleta clicada
+  - [ ] añadir o quitar la clase en el HTML de las paletas clicadas
+- [ ] Localstorage
+- [ ] Filtrar por nombre
+  - [ ] Definir el input del buscador
+  - [ ] Escuchar un evento sobre el input,
+  - [ ] filtrar
+  - [ ] pintar html los datos filtrados */
